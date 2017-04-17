348428cddee468db0d48c95364a8bdd62dbccc68
何加一 <JAY@oonne.com>
2017-04-17 13:58:34 +0800

    分离导航栏

--- project/frontend/views/layouts/main.php
+++ project/frontend/views/layouts/main.php
@@ -4,13 +4,10 @@
 /* @var $content string */
 
 use yii\helpers\Html;
-use yii\bootstrap\Nav;
-use yii\bootstrap\NavBar;
 use yii\widgets\Breadcrumbs;
 use frontend\assets\AppAsset;
 use common\widgets\Alert;
 use oonne\scrollTop\ScrollTop;
-use common\models\Category;
 
 AppAsset::register($this);
 ?>
@@ -26,46 +23,20 @@ AppAsset::register($this);
 </head>
 <body>
 <?php $this->beginBody() ?>
+<?= $this->render('_nav') ?>
 
-    <?php
-    NavBar::begin([
-        'brandLabel' => Yii::$app->params['blogName'],
-        'brandUrl' => Yii::$app->homeUrl,
-        'options' => [
-            'class' => 'navbar-inverse navbar-fixed-top ring-nav',
-        ],
-    ]);
-
-    $menuItems = [];
-    // array_push($menuItems, ['label' => '全部', 'url' => ['/site/index']]);
-    $categories = Category::getCategoryList();
-    foreach($categories as $category){
-        $category_name = $category['category_name'];
-        $category_id = $category['id'];
-        array_push($menuItems, ['label' => $category_name, 'url' => ['/site/category', 'id' => $category_id] ]);
-    };
-    echo Nav::widget([
-        'options' => ['class' => 'navbar-nav navbar-right'],
-        'items' => $menuItems,
-    ]);
-    
-    NavBar::end();
-    ?>
-
-    <div class="wrap">
-        <div class="container">
-            <?= Breadcrumbs::widget([
-                'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-            ]) ?>
-            <?= Alert::widget() ?>
-            <?= $content ?>
-        </div>
+<div class="wrap">
+    <div class="container">
+        <?= Breadcrumbs::widget([
+            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+        ]) ?>
+        <?= Alert::widget() ?>
+        <?= $content ?>
     </div>
+</div>
 
-    <?= $this->render('_foot') ?>
-
-    <?= ScrollTop::widget() ?>
-
+<?= $this->render('_foot') ?>
+<?= ScrollTop::widget() ?>
 <?php $this->endBody() ?>
 </body>
 </html>
