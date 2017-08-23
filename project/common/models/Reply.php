80ead330cb917448fbf3b04928ca7ae746f56758
victorruan <250069802@qq.com>
2017-08-23 17:53:04 +0800

    增加两个评论相关的ar类

---
+++ project/common/models/Reply.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "reply".
+ *
+ * @property integer $id
+ * @property integer $comment_id
+ * @property string $reply
+ * @property integer $status
+ * @property integer $created_at
+ * @property integer $updated_at
+ */
+class Reply extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'reply';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['comment_id', 'created_at', 'updated_at'], 'required'],
+            [['comment_id', 'status', 'created_at', 'updated_at'], 'integer'],
+            [['reply'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'comment_id' => 'Comment ID',
+            'reply' => 'Reply',
+            'status' => 'Status',
+            'created_at' => 'Created At',
+            'updated_at' => 'Updated At',
+        ];
+    }
+}
