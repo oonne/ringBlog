5159d54b3d729d7f32aa95015c7e2eeb715385c0
JAY@oonne.com <JAY@oonne.com>
2018-06-19 15:27:09 +0800

    查看博客页面，追加本文链接功能，便于直接在后台复制

--- project/backend/views/blogsuper/view.php
+++ project/backend/views/blogsuper/view.php
@@ -7,6 +7,12 @@ $this->title = $model->blog_title;
 <div class="row blog-content">
 	<h1 class="page-header"><?= Html::encode($this->title) ?></h1>
 	<p>
+		<?php 
+			echo '链接：';
+            echo Html::a(Yii::$app->params['blogUrl'].'site/blog?id='.$model->id, [Yii::$app->params['blogUrl'].'site/blog?id='.$model->id]);
+		?>
+	</p>
+	<p>
 		<?php echo '分类：'.$model->category->category_name ?>	
 	</p>
 	<?php echo $model->blog_content ?>
